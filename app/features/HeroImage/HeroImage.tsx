82ed12805e48d017d56d6f1fe1dcbd08c0bb553f
Milos Milovanovic <53410962+MMilos6@users.noreply.github.com>
2024-07-29 17:43:51 +0200

    feat: image builder

--- app/features/HeroImage/HeroImage.tsx
+++ app/features/HeroImage/HeroImage.tsx
@@ -1,5 +1,8 @@
-import classNames from "classnames";
-import { IHeroImageProps } from "./types/heroImageType";
+'use client';
+import Link from 'next/link';
+import classNames from 'classnames';
+import { urlFor } from '@/app/utils/imageBuilder';
+import { IHeroImageProps } from './types/heroImageType';
 
 export const HeroImage = ({
     title,
@@ -12,29 +15,51 @@ export const HeroImage = ({
     isFullHeight,
 }: IHeroImageProps) => {
 
-    const heroImageClassName = classNames(className, 'hero-image', 
+    const heroImageClassName = classNames(className, 'hero-image',
         {
             'hero-image-no-full-height': !isFullHeight,
             'hero-image-fullheader-height': isFullHeader
         }
     );
 
+    const desktopImageSrc = imageSrc ? urlFor(imageSrc, 1900, 500) : 'none';
+    const mobileImageSrc = imageSrc ? urlFor(imageSrc, 400, 400) : 'none';
+
     return (
-        <section className={heroImageClassName} style={{ backgroundImage: `url(${imageSrc || "none"})` }} aria-label={imageAlt}>
-            <div className="container">
-                <div className="row">
-                    <div className="d-flex flex-wrap flex-column justify-content-center align-items-center p-4 text-white">
-                        <h2 className="text-white fw-bold">
-                            {title}
-                        </h2>
-                        {ctaLabel && ctaLink && (
-                            <a href={`${ctaLink.type}/${ctaLink.slug}`} className="btn btn-primary mb-2">
-                                {ctaLabel}
-                            </a>
-                        )}
+        <>
+            <link rel='preload' as='image' href={mobileImageSrc} />
+            <link rel='preload' as='image' href={desktopImageSrc} />
+            <section className={heroImageClassName} aria-label={imageAlt}>
+                <picture>
+                    <source srcSet={mobileImageSrc} media='(max-width: 991px)' />
+                    <source srcSet={desktopImageSrc} media='(min-width: 992px)' />
+                    <img
+                        loading='lazy'
+                        aria-hidden={true}
+                        className='bgImage'
+                        src={desktopImageSrc}
+                        alt={imageAlt || 'Hero background image'}
+                    />
+                </picture>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='d-flex flex-wrap flex-column justify-content-center align-items-center p-4 text-white'>
+                            <h2 className='text-white fw-bold'>
+                                {title}
+                            </h2>
+                            {ctaLabel && ctaLink && (
+                                <Link
+                                    aria-label={ctaLabel}
+                                    className='btn btn-primary mb-2'
+                                    href={`${ctaLink.type}/${ctaLink.slug}`}
+                                >
+                                    {ctaLabel}
+                                </Link>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
